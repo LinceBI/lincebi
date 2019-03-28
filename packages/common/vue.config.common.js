8f199eed72ec8e561672d635db6e94e3e00199ff
Héctor Molinero <hector.molinero@stratebi.com>
2019-03-28 17:23:00 +0100

    Añadido soporte para cargar URLs en los componentes de bootstrap-vue

--- packages/common/vue.config.common.js
+++ packages/common/vue.config.common.js
@@ -4,6 +4,29 @@ module.exports = {
 	integrity: true,
 	productionSourceMap: false,
 	devServer: { historyApiFallback: false },
+	chainWebpack: config => {
+		// Project relative image URLs for BootstrapVue custom components.
+		// See: https://bootstrap-vue.js.org/docs/reference/images/
+		config.module
+			.rule('vue')
+			.use('vue-loader')
+			.loader('vue-loader')
+			.tap(options => {
+				options['transformAssetUrls'] = {
+					img: 'src',
+					image: 'xlink:href',
+					'b-img': 'src',
+					'b-img-lazy': ['src', 'blank-src'],
+					'b-card': 'img-src',
+					'b-card-img': 'img-src',
+					'b-card-img-lazy': ['src', 'blank-src'],
+					'b-carousel-slide': 'img-src',
+					'b-embed': 'src'
+				}
+
+				return options;
+			});
+	},
 	css: {
 		loaderOptions: {
 			sass: {
