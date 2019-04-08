3561976d2371506881a867b18523dd47b6fb6881
Héctor Molinero <hector.molinero@stratebi.com>
2019-04-08 16:48:07 +0200

    Añadidos métodos fileToDataURI y imageToDataURI

--- packages/common/src/main.js
+++ packages/common/src/main.js
@@ -1,11 +1,13 @@
 import './scss/main.scss';
 
 export { default as canAdminister } from './canAdminister';
+export { default as fileToDataURI } from './fileToDataURI';
 export { default as formData } from './formData';
 export { default as getContextPath } from './getContextPath';
 export { default as getParameter } from './getParameter';
 export { default as getUnicodeCharacter } from './fac/getUnicodeCharacter';
 export { default as getUserSetting } from './getUserSetting';
+export { default as imageToDataURI } from './imageToDataURI';
 export { default as insertIf } from './insertIf';
 export { default as invokeWhen } from './invokeWhen';
 export { default as safeJSON } from './safeJSON';
