5c8feee5d90135a28f0dac1ce4d96bcc0965f247
Héctor Molinero <hector.molinero@stratebi.com>
2019-04-08 15:56:21 +0200

    Implementados métodos getUserSetting y setUserSetting

---
+++ packages/common/src/setUserSetting.js
@@ -0,0 +1,19 @@
+import fetch from 'unfetch';
+import getContextPath from './getContextPath';
+
+export default async (key, value = '') => {
+	const contextPath = await getContextPath();
+	const endpoint = `${contextPath}api/user-settings/${key}`;
+	const response = await fetch(endpoint, {
+		method: 'POST',
+		headers: { 'Content-Type': 'text/plain' },
+		body: value
+	});
+
+	if (response.status === 200) {
+		const value = await response.text();
+		return value === 'null' ? '' : value;
+	}
+
+	return null;
+};
