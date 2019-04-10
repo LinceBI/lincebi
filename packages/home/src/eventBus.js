7818ad392f28fd9ce3f2dc280b8568e7ee91c6b5
Héctor Molinero <hector.molinero@stratebi.com>
2019-04-10 17:40:03 +0200

    Añadido método emitWhen al bus de eventos

--- packages/home/src/eventBus.js
+++ packages/home/src/eventBus.js
@@ -1,2 +1,13 @@
 import Vue from 'vue';
-export default new Vue();
+
+import invokeWhen from '@stratebi/biserver-customization-common/src/invokeWhen';
+
+const eventBus = new Vue();
+eventBus.$emitWhen = (eventName, ...args) => {
+	invokeWhen(
+		() => eventBus._events[eventName],
+		() => eventBus.$emit(eventName, ...args)
+	);
+};
+
+export default eventBus;
