4169da574e11ff419566d37b3693907ac42dfa11
Héctor Molinero <hector.molinero@stratebi.com>
2019-04-12 09:02:37 +0200

    Reducida espera por defecto de invokeWhen

--- packages/common/src/invokeWhen.js
+++ packages/common/src/invokeWhen.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash/throttle';
 
-export default (condFunc, invokeFunc, { wait = 100, timeout = 10000 } = {}) => {
+export default (condFunc, invokeFunc, { wait = 50, timeout = 10000 } = {}) => {
 	const start = performance.now();
 	const tryInvoke = throttle(() => {
 		if (performance.now() - start <= timeout) {
