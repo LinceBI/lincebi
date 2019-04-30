1734883c8962340ced360379516dc81e337f38ec
Héctor Molinero <hector.molinero@stratebi.com>
2019-04-30 17:00:40 +0200

    Añadidos reconocer más extensiones de archivos en Font Awesome

---
+++ packages/common/src/fontawesome/faFileXml.js
@@ -0,0 +1,16 @@
+import getFaUnicodeCharacter from './getUnicodeCharacter';
+
+import { width, height, ligatures, svgPathData } from './faToolUnknown';
+export { width, height, ligatures, svgPathData };
+
+export const prefix = 'fac';
+export const iconName = 'file-xml';
+export const unicode = getFaUnicodeCharacter();
+
+export const definition = {
+	prefix: prefix,
+	iconName: iconName,
+	icon: [width, height, ligatures, unicode, svgPathData]
+};
+
+export { definition as faFileXml };
