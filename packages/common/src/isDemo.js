4523ab99930abe68a3c2c5073ea5c0c6f38d3ee1
Héctor Molinero <hector.molinero@stratebi.com>
2019-06-10 10:59:58 +0200

    Ajustada comprobación de demo

--- packages/common/src/isDemo.js
+++ packages/common/src/isDemo.js
@@ -1,3 +1,4 @@
 const ENV = process.env.VUE_APP_ENV;
-const DEMO_HOSTNAME = process.env.VUE_APP_DEMO_HOSTNAME || 'demo.stratebi.com';
-export default ENV === 'demo' || DEMO_HOSTNAME === window.location.hostname;
+export default ENV === 'demo' ||
+	window.location.hostname === 'demo.stratebi.com' ||
+	window.location.hostname === 'local.demo.stratebi.com';
