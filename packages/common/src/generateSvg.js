654b19ce648f23b41d29c8d72272624a089cbc38
Héctor <hector.molinero@stratebi.com>
2019-11-07 13:42:01 +0100

    Modificado tono de color de SVGs generados

--- packages/common/src/generateSvg.js
+++ packages/common/src/generateSvg.js
@@ -3,12 +3,18 @@ import jdenticon from 'jdenticon';
 const maxCacheSize = 500;
 const cache = new Map();
 
-export default (value = '', padding = 0.08) => {
+export default (value = '') => {
 	if (cache.has(value)) {
 		return cache.get(value);
 	}
 
-	const svg = jdenticon.toSvg(value, 256, padding);
+	const svg = jdenticon.toSvg(value, 256, {
+		padding: 0.08,
+		lightness: {
+			color: [0.8, 0.8],
+			grayscale: [0.8, 0.8]
+		}
+	});
 	const dataURI = `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
 	cache.set(value, dataURI);
 
