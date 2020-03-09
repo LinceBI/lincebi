1665fcd70cde4461d49e38fc90506229c61bce56
Héctor <hector.molinero@stratebi.com>
2020-03-09 09:00:48 +0100

    Añadido método alternativo para comprobar la existencia de STPivot

--- packages/common/src/biserver/plugins/isStpivotInstalled.js
+++ packages/common/src/biserver/plugins/isStpivotInstalled.js
@@ -1,8 +1,12 @@
 import fetch from 'unfetch';
 
 import getContextPath from '../getContextPath';
+import getRegisteredPlugins from '../getRegisteredPlugins';
 
 export default async () => {
+	const registeredPlugins = await getRegisteredPlugins();
+	if (registeredPlugins.includes('stpivot')) return true;
+
 	const contextPath = await getContextPath();
 	const resource = 'stpivot/build.json';
 	const endpoint = `${contextPath}${resource}`;
