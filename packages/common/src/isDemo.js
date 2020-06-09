641c14235329130a8819e72fc52779eb21590c77
Héctor <hector.molinero@stratebi.com>
2020-06-09 10:55:19 +0200

    Eliminado js sobrante

--- packages/common/src/isDemo.js
+++
@@ -1,6 +0,0 @@
-const ENV = process.env.VUE_APP_ENV;
-export default ENV === 'demo' ||
-	window.location.hostname === 'demo.lincebi.com' ||
-	window.location.hostname === 'demo.lincebi.localdomain' ||
-	window.location.hostname === 'demo.stratebi.com' ||
-	window.location.hostname === 'demo.stratebi.localdomain';
