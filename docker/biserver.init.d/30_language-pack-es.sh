6673cc5289c680aa3316c141c4d74861af18d909
Héctor <hector.molinero@stratebi.com>
2021-03-01 14:41:05 +0100

    Instalar paquete de idioma español

---
+++ docker/biserver.init.d/30_language-pack-es.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+export LC_ALL=C
+
+RELEASE='9.1-20.10.13'
+URL="https://repo.stratebi.com/repository/lincebi-raw/pentaho-language-packs/languagePack_es-${RELEASE:?}.zip"
+
+curl -fsSL "${URL:?}" > "${BISERVER_HOME:?}"/"${SOLUTIONS_DIRNAME:?}"/system/language-pack-es.zip
+(cd "${BISERVER_HOME:?}"/"${SOLUTIONS_DIRNAME:?}"/system/ && unzip -qo ./language-pack-es.zip && rm -f ./language-pack-es.zip)
+
+/usr/share/biserver/bin/kitchen.sh -level=Error -file="${BISERVER_HOME:?}"/"${SOLUTIONS_DIRNAME:?}"/system/languagePack_es/endpoints/kettle/admin/installpack.kjb
