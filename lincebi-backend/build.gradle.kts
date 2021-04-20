e48abf1ffff2a18cde316244d244285b0b2fb323
Héctor <hector.molinero@stratebi.com>
2021-04-20 11:33:31 +0200

    Actualización de dependencias

--- lincebi-backend/build.gradle.kts
+++ lincebi-backend/build.gradle.kts
@@ -14,24 +14,24 @@ repositories {
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
 	implementation("com.microsoft.azure:msal4j:1.9.1")
-	implementation("com.networknt:json-schema-validator:1.0.49")
+	implementation("com.networknt:json-schema-validator:1.0.52")
 	implementation("commons-io:commons-io:2.8.0")
 	implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
-	implementation("org.apache.commons:commons-lang3:3.11")
+	implementation("org.apache.commons:commons-lang3:3.12.0")
 	implementation("org.codehaus.enunciate:enunciate-core-annotations:1.31")
-	implementation("org.ehcache:ehcache:3.9.1")
+	implementation("org.ehcache:ehcache:3.9.2")
 	implementation("org.slf4j:slf4j-log4j12:1.7.30")
-	implementation("org.springframework:spring-web:5.3.3")
+	implementation("org.springframework:spring-web:5.3.6")
 	implementation("org.thymeleaf:thymeleaf:3.0.12.RELEASE")
 
-	compileOnly("org.pentaho:commons-database-model:8.3.0.21-1196") { isTransitive = false }
-	compileOnly("org.pentaho:pentaho-metadata:8.3.0.21-1196") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-api:8.3.0.21-1196") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-core:8.3.0.21-1196") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-extensions:8.3.0.21-1196") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-repository:8.3.0.21-1196") { isTransitive = false }
+	compileOnly("org.pentaho:commons-database-model:8.3.0.22-1241") { isTransitive = false }
+	compileOnly("org.pentaho:pentaho-metadata:8.3.0.22-1241") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-api:8.3.0.22-1241") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-core:8.3.0.22-1241") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-extensions:8.3.0.22-1241") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-repository:8.3.0.22-1241") { isTransitive = false }
 
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
