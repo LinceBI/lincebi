200441319636fb8517bf016a1628e017bfa22c29
Héctor <hector.molinero@stratebi.com>
2021-10-13 11:48:37 +0200

    Actualizadas dependencias del backend

--- lincebi-backend/build.gradle.kts
+++ lincebi-backend/build.gradle.kts
@@ -14,9 +14,9 @@ repositories {
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
 	implementation("com.microsoft.azure:msal4j:1.11.0")
-	implementation("com.networknt:json-schema-validator:1.0.58")
+	implementation("com.networknt:json-schema-validator:1.0.61")
 	implementation("commons-io:commons-io:2.11.0")
 	implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
 	implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
@@ -24,7 +24,7 @@ dependencies {
 	implementation("org.codehaus.enunciate:enunciate-core-annotations:1.31")
 	implementation("org.ehcache:ehcache:3.9.6")
 	implementation("org.slf4j:slf4j-log4j12:1.7.32")
-	implementation("org.springframework:spring-web:5.3.9")
+	implementation("org.springframework:spring-web:5.3.10")
 	implementation("org.thymeleaf:thymeleaf:3.0.12.RELEASE")
 
 	compileOnly("org.pentaho:commons-database-model:8.3.0.23-1295") { isTransitive = false }
@@ -34,8 +34,8 @@ dependencies {
 	compileOnly("pentaho:pentaho-platform-extensions:8.3.0.23-1295") { isTransitive = false }
 	compileOnly("pentaho:pentaho-platform-repository:8.3.0.23-1295") { isTransitive = false }
 
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.build {
