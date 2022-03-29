6015bfda6a2efa6160d793824a2d42fb2de65cf2
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2022-03-29 09:26:21 +0200

    Actualizadas dependencias del backend

--- lincebi-backend/build.gradle.kts
+++ lincebi-backend/build.gradle.kts
@@ -15,19 +15,19 @@ repositories {
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
-	implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
-	implementation("com.microsoft.azure:msal4j:1.11.2")
-	implementation("com.networknt:json-schema-validator:1.0.66")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+	implementation("com.microsoft.azure:msal4j:1.11.3")
+	implementation("com.networknt:json-schema-validator:1.0.68")
 	implementation("commons-io:commons-io:2.11.0")
 	implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
 	implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
 	implementation("org.apache.commons:commons-lang3:3.12.0")
 	implementation("org.codehaus.enunciate:enunciate-core-annotations:1.31")
-	implementation("org.ehcache:ehcache:3.9.9")
+	implementation("org.ehcache:ehcache:3.10.0")
 	implementation("org.slf4j:slf4j-reload4j:1.7.36")
-	implementation("org.springframework:spring-web:5.3.16")
+	implementation("org.springframework:spring-web:5.3.17")
 	implementation("org.thymeleaf:thymeleaf:3.0.15.RELEASE")
 
 	compileOnly("org.pentaho:commons-database-model:8.3.0.26-1500") { isTransitive = false }
