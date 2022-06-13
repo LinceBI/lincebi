ca02dffa7798f6b4e8b306f505def7b37f571b50
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2022-06-13 13:22:55 +0200

    Actualizados paquetes de idioma

--- docker/biserver.init.d/30_language-pack-es.sh
+++ docker/biserver.init.d/30_language-pack-es.sh
@@ -3,7 +3,7 @@
 set -eu
 export LC_ALL=C
 
-VERSION='9.2-21.09.13'
+VERSION='9.3-22.06.13'
 URL="https://repo.stratebi.com/repository/lincebi-raw/pentaho-language-packs/languagePack_es-${VERSION:?}.zip"
 
 curl -fsSL "${URL:?}" > "${BISERVER_HOME:?}"/"${SOLUTIONS_DIRNAME:?}"/system/language-pack-es.zip
