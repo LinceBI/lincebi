b70e5072528c3d7a67c1a9b82d44e056f4fdf002
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2022-09-08 15:17:12 +0200

    Actualizado STSearch en despliegue de desarrollo

--- docker/biserver.init.d/30_stsearch.sh
+++ docker/biserver.init.d/30_stsearch.sh
@@ -3,7 +3,7 @@
 set -eu
 export LC_ALL=C
 
-RELEASE='1.8.3'; SNAPSHOT=''
+RELEASE='1.8.5'; SNAPSHOT=''
 URL="https://repo.stratebi.com/repository/lincebi-mvn/com/stratebi/lincebi/stsearch/${RELEASE:?}${SNAPSHOT:+-SNAPSHOT}/stsearch-${RELEASE:?}${SNAPSHOT:+-}${SNAPSHOT?}.zip"
 
 curl -fsSL "${URL:?}" > "${BISERVER_HOME:?}"/"${SOLUTIONS_DIRNAME:?}"/system/stsearch.zip
