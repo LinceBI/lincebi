7a67ea3464f905b7c8bb08a9c8551c3209332bf3
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2023-03-21 15:52:17 +0100

    Actualización de dependencias

--- lincebi-backend/build.gradle.kts
+++ lincebi-backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("java")
-	id("com.github.johnrengelman.shadow") version "8.1.0"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "${project.property("group")}"
@@ -26,16 +26,16 @@ dependencies {
 	implementation("org.apache.commons:commons-lang3:3.12.0")
 	implementation("org.codehaus.enunciate:enunciate-core-annotations:1.31")
 	implementation("org.ehcache:ehcache:3.10.8")
-	implementation("org.slf4j:slf4j-reload4j:2.0.6")
+	implementation("org.slf4j:slf4j-reload4j:2.0.7")
 	implementation("org.springframework:spring-web:5.3.25")
 	implementation("org.thymeleaf:thymeleaf:3.1.1.RELEASE")
 
-	compileOnly("org.pentaho:commons-database-model:9.3.0.2-617") { isTransitive = false }
-	compileOnly("org.pentaho:pentaho-metadata:9.3.0.2-617") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-api:9.3.0.2-617") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-core:9.3.0.2-617") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-extensions:9.3.0.2-617") { isTransitive = false }
-	compileOnly("pentaho:pentaho-platform-repository:9.3.0.2-617") { isTransitive = false }
+	compileOnly("org.pentaho:commons-database-model:9.3.0.3-702") { isTransitive = false }
+	compileOnly("org.pentaho:pentaho-metadata:9.3.0.3-702") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-api:9.3.0.3-702") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-core:9.3.0.3-702") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-extensions:9.3.0.3-702") { isTransitive = false }
+	compileOnly("pentaho:pentaho-platform-repository:9.3.0.3-702") { isTransitive = false }
 
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
