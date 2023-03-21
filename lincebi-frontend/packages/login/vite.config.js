92a2417dbb5b7872b3d35096815b18f3d8c69e26
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2023-03-21 15:10:19 +0100

    Formato

--- lincebi-frontend/packages/login/vite.config.js
+++ lincebi-frontend/packages/login/vite.config.js
@@ -5,17 +5,19 @@ import merge from 'lodash/merge';
 
 import viteConfigCommon from '@lincebi/frontend-common/vite.config.js';
 
-export default defineConfig((config) => merge(viteConfigCommon(config), {
-	base: config.command === 'serve' ? '/lincebi/Login/' : '',
-	resolve: {
-		alias: {
-			'@/': fileURLToPath(new URL('./src/', import.meta.url))
+export default defineConfig((config) =>
+	merge(viteConfigCommon(config), {
+		base: config.command === 'serve' ? '/lincebi/Login/' : '',
+		resolve: {
+			alias: {
+				'@/': fileURLToPath(new URL('./src/', import.meta.url)),
+			},
 		},
-	},
-	server: {
-		port: 3031,
-	},
-	preview: {
-		port: 8081,
-	},
-}));
+		server: {
+			port: 3031,
+		},
+		preview: {
+			port: 8081,
+		},
+	})
+);
