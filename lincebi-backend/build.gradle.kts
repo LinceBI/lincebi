2085e58f56ad2c2d3333e3e3c63667f2165dfb55
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2023-07-17 12:54:25 +0200

    Actualización de dependencias

--- lincebi-backend/build.gradle.kts
+++ lincebi-backend/build.gradle.kts
@@ -15,19 +15,19 @@ repositories {
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.0")
-	implementation("com.fasterxml.jackson.core:jackson-core:2.15.0")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
-	implementation("com.microsoft.azure:msal4j:1.13.8")
-	implementation("com.networknt:json-schema-validator:1.0.80")
-	implementation("commons-io:commons-io:2.11.0")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+	implementation("com.microsoft.azure:msal4j:1.13.9")
+	implementation("com.networknt:json-schema-validator:1.0.86")
+	implementation("commons-io:commons-io:2.13.0")
 	implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
 	implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
 	implementation("org.apache.commons:commons-lang3:3.12.0")
 	implementation("org.codehaus.enunciate:enunciate-core-annotations:1.31")
 	implementation("org.ehcache:ehcache:3.10.8")
 	implementation("org.slf4j:slf4j-reload4j:2.0.7")
-	implementation("org.springframework:spring-web:5.3.27")
+	implementation("org.springframework:spring-web:5.3.29")
 	implementation("org.thymeleaf:thymeleaf:3.1.1.RELEASE")
 
 	compileOnly("org.pentaho:commons-database-model:9.3.0.4-717") { isTransitive = false }
@@ -37,8 +37,8 @@ dependencies {
 	compileOnly("pentaho:pentaho-platform-extensions:9.3.0.4-717") { isTransitive = false }
 	compileOnly("pentaho:pentaho-platform-repository:9.3.0.4-717") { isTransitive = false }
 
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
 tasks.build {
